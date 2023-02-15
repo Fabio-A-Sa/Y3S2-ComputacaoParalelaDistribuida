a26550f1d74eef4012b4f0162184fd3dbdfa00c1
Fabio-A-Sa <up202007658@fe.up.pt>
2023-02-15 11:44:16 +0000

    second algorithm

--- Exercises/matrix.cpp
+++ Exercises/matrix.cpp
@@ -72,8 +72,59 @@ void OnMult(int m_ar, int m_br)
 
 // add code here for line x line matriz multiplication
 void OnMultLine(int m_ar, int m_br)
-{
-    
+{   	
+	SYSTEMTIME Time1, Time2;
+	
+	char st[100];
+	int i, j, k;
+
+	double *pha, *phb, *phc;
+	
+    pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+	phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+	for(i=0; i<m_ar; i++)
+		for(j=0; j<m_ar; j++)
+			pha[i*m_ar + j] = (double)1.0;
+
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phb[i*m_br + j] = (double)(i+1);
+	
+	for(i=0; i<m_br; i++)
+		for(j=0; j<m_br; j++)
+			phc[i*m_br + j] = (double)0.0;
+
+    Time1 = clock();
+
+	for(i=0; i<m_ar; i++)
+	{	for( k=0; k<m_ar; k++ )
+		{
+			for( j=0; j<m_br; j++)
+			{	
+				phc[i*m_ar+j] += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+		}
+	}
+
+
+    Time2 = clock();
+	sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+	cout << st;
+
+	// display 10 elements of the result matrix tto verify correctness
+	cout << "Result matrix: " << endl;
+	for(i=0; i<1; i++)
+	{	for(j=0; j<min(10,m_br); j++)
+			cout << phc[j] << " ";
+	}
+	cout << endl;
+
+    free(pha);
+    free(phb);
+    free(phc);
+	
     
 }
 
@@ -108,7 +159,6 @@ void init_papi() {
 
 int main (int argc, char *argv[])
 {
-
 	char c;
 	int lin, col, blockSize;
 	int op;
@@ -117,23 +167,18 @@ int main (int argc, char *argv[])
   	long long values[2];
   	int ret;
 	
-
 	ret = PAPI_library_init( PAPI_VER_CURRENT );
 	if ( ret != PAPI_VER_CURRENT )
 		std::cout << "FAIL" << endl;
 
-
 	ret = PAPI_create_eventset(&EventSet);
 		if (ret != PAPI_OK) cout << "ERROR: create eventset" << endl;
-
 
 	ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
 	if (ret != PAPI_OK) cout << "ERROR: PAPI_L1_DCM" << endl;
 
-
 	ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
 	if (ret != PAPI_OK) cout << "ERROR: PAPI_L2_DCM" << endl;
-
 
 	op=1;
 	do {
